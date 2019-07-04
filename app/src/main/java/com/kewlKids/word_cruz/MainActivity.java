1bdc61a0283c8fa54754809ee21aae1591cf8569
Kevin <kevin.wangsentbon@gmail.com>
2019-07-04 08:38:27 +0800

    Test commit

--- app/src/main/java/com/kewlKids/word_cruz/MainActivity.java
+++ app/src/main/java/com/kewlKids/word_cruz/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {

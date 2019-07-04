73267efff89d5a29c4ffa9a368a4c46dd0712280
Kevin <kevin.wangsentbon@gmail.com>
2019-07-04 09:30:14 +0800

    Test commit 2

--- app/src/main/java/com/kewlKids/word_cruz/MainActivity.java
+++ app/src/main/java/com/kewlKids/word_cruz/MainActivity.java
@@ -20,10 +20,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
-
-
         
         FloatingActionButton fab = findViewById(R.id.fab);
 

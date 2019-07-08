22e5e07bf6082779389ddb6940a40311b34ae10e
Jeffletroll <jeffletroll@gmai.com>
2019-07-08 16:01:05 -0400

    This is the how to draw something.

--- app/src/main/java/com/kewlKids/word_cruz/CWordCruZSquareBuilder.java
+++ app/src/main/java/com/kewlKids/word_cruz/CWordCruZSquareBuilder.java
@@ -1,5 +1,21 @@
 package com.kewlKids.word_cruz;
-
+        import android.os.Bundle;
+        import android.app.Activity;
+        import android.view.Menu;
+        import android.content.Context;
+        import android.graphics.Canvas;
+        import android.graphics.Color;
+        import android.graphics.Paint;
+        import android.view.View;
 public class CWordCruZSquareBuilder {
 
+
+    Paint paint = new Paint();
+
+
+    public void onDraw(Canvas canvas) {
+        paint.setColor(Color.RED);
+        paint.setStrokeWidth(3);
+        canvas.drawRect(30, 30, 80, 80, paint);
+    }
 }

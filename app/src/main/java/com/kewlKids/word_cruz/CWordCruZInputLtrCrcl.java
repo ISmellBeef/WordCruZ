6839fea75d3131263235d2c474f7ccdda3f41c6d
Julian Ma <julian.m.officiel@gmail.com>
2019-07-29 21:11:46 -0400

    white ltr circle julian

--- app/src/main/java/com/kewlKids/word_cruz/CWordCruZInputLtrCrcl.java
+++ app/src/main/java/com/kewlKids/word_cruz/CWordCruZInputLtrCrcl.java
@@ -2,9 +2,13 @@ package com.kewlKids.word_cruz;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.view.View;
 
 public class CWordCruZInputLtrCrcl extends View {
+
+    Paint paint = new Paint();
 
     CWordCruZInputLtrCrcl(Context c) {
         super(c);
@@ -13,6 +17,8 @@ public class CWordCruZInputLtrCrcl extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
+        paint.setAntiAlias(true);
+        paint.setColor(Color.WHITE);
+        canvas.drawCircle(250, 250, 250, paint);
     }
 }

ef1922f173112aeb9caadcb2c09c7105df7e0edf
Kevin <kevin.wangsentbon@gmail.com>
2019-08-18 23:07:56 +0800

    ActivityMain test 1

--- app/src/main/java/com/kewlKids/word_cruz/CWordCruZManager.java
+++ app/src/main/java/com/kewlKids/word_cruz/CWordCruZManager.java
@@ -1,15 +1,39 @@
 package com.kewlKids.word_cruz;
 import java.util.Random;
 public class CWordCruZManager {
-    public int words (int list[])[]
+    public int Randomwords (int list[],int wordnum, int maxletter,int minletter)[]
     {
-            Random rand = new Random(list.length);
-            int wordnum = 6;
+          //  Random rand = new Random(list.length);
             int words[] = new int[wordnum];
+            /*
             for (int a = 0; a < wordnum;a++)
             {
                 words[a] = rand.nextInt();
+                for (int i = 0; i <)
             }
-            return words;
+            */return words;
+    }
+    public void charUnion (String word1,String word2,int maxunion)
+    {
+     /*   char wc1 [] = new char[word1.length()];
+        char wc2 [] = new char[word2.length()];
+        for (int a = 0;a < word1.length();a++)
+            wc1[a] = word1.charAt(a);
+        for (int a = 0;a < word1.length();a++)
+            wc2[a] = word2.charAt(a);
+        int n = 0;
+        int min = (wc1.length < wc2.length)? wc1.length:wc2.length;
+        char intersection [] = new char [min];
+        for (int i = 0; i < wc1.length;i++,j++)
+        {
+            for (int j = 0; j < wc1.length;j++)
+            {
+                if (wc1[i] == wc2[j])
+                {
+                    intersection[n] = wc1[i];
+                    n++;
+                }
+            }
+        }*/
     }
 }

6567671c5f185e7c72481dc72f71b0a3c7ff9ca3
Julian Ma <julian.m.officiel@gmail.com>
2019-08-26 17:45:08 -0400

    copying my codes

--- app/src/main/java/com/kewlKids/word_cruz/CWordCruZInputLtrCrcl.java
+++ app/src/main/java/com/kewlKids/word_cruz/CWordCruZInputLtrCrcl.java
@@ -2,7 +2,12 @@ package com.kewlKids.word_cruz;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
 import android.view.View;
+
+
 
 public class CWordCruZInputLtrCrcl extends View {
 
@@ -10,9 +15,34 @@ public class CWordCruZInputLtrCrcl extends View {
         super(c);
     }
 
+    Paint paint = new Paint();
+
     @Override
     protected void onDraw(Canvas canvas) {
+        //white circle
         super.onDraw(canvas);
+        //paint.setAntiAlias(true);
+        //paint.setColor(Color.WHITE);
+        //paint.setStrokeWidth(5);
+        //canvas.drawCircle(450, 450, 450, paint);
 
+        //white circle
+        int x = getWidth();
+        int y = getHeight();
+        int radius = 580;
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.parseColor("#ffffff"));
+        canvas.drawCircle(725, 1620, radius, paint);
+
+        //black border
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(10);
+        paint.setColor(Color.BLACK);
     }
+
+    //protected void onCreate(Bundle savedInstanceState){
+    //super.onCreate(savedInstanceState);
+
+    //}
 }
